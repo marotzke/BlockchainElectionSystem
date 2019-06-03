69d555c28554aa1b3ca9428de42cfe4af982647a
Matheus Marotzke <matheusmarotzke@Matheuss-MacBook-Pro.local>
2019-06-02 21:45:46 -0300

    results and vote ui ready

--- webapp/src/components/Results/Results.js
+++ webapp/src/components/Results/Results.js
@@ -1,70 +1,116 @@
 import React, { Component } from 'react'
-import { Header, Image, Table } from 'semantic-ui-react'
+import { Header, Image, Table, Card, Icon, Label} from 'semantic-ui-react'
+import styles from './Results.module.scss'; 
+
+const names = ["matthew","mark","molly"]
+const data1 = [
+    {
+        "id": 0,
+        "name": "Joao",
+        "party": "PT",
+        "voteCount": 112
+    },
+    {
+        "id": 1,
+        "name": "Otavio",
+        "party": "PSDB",
+        "voteCount": 87
+    },
+    {
+        "id": 2,
+        "name": "Marcos",
+        "party": "CINSPER",
+        "voteCount": 83
+    },
+]
 
 export default class Results extends Component {
     constructor(props){
         super(props)
         this.state = {
-            candidate1 : {
-                name: "Matheus Marotzke",
-                party:"INSCOMP",
-                voteCount:2
-            },
-            candidate2 : {
-                name: "Marcelo Andrade",
-                party:"CISP",
-                voteCount:21
-            }
+            candidates : null
         }
     }
 
     componentWillMount() {
+        // this.setState({candidates: data1}) // UNCOMMENT TO TEST WITHOUT SERVER-SIDE 
         fetch('http://localhost:8545/results')
-          .then(response => response.json())
-          .then(data => 
-            console.log(data)
-            // this.setState({ data })
-            
+        .then(response => response.json())
+        .then(data => 
+            this.setState({candidates: data})
             );
       }
 
     render() {
-        return (
-            <Table basic='very' celled collapsing>
+        let main = null
+        let candidateHeader = null
+        if (this.state.candidates != null) {
+        
+        let max = Math.max.apply(Math, this.state.candidates.map(function(o) { return o.voteCount; }))
+        const candidate = this.state.candidates.find( c => c.voteCount === parseInt(max,10) );
+        candidateHeader = 
+        <Card>
+          <Image 
+            fluid
+            label={{ as: 'a', corner: 'left', color:"green", icon: 'winner' }}
+            src='https://react.semantic-ui.com/images/avatar/large/matthew.png'
+            wrapped 
+            ui={false} />
+          <Card.Content>
+          
+            <Card.Header>{candidate.name}</Card.Header>
+            <Card.Meta>
+              <span className='date'>{candidate.party}</span>
+            </Card.Meta>
+            <Card.Content extra>
+                <a>
+                    <Icon name='user' />
+                    NÚMERO DE VOTOS: {candidate.voteCount}
+                </a>
+            </Card.Content>
+            <Label as='a' color='green' ribbon className={styles.tag}>
+            Candidato Eleito
+          </Label>
+          </Card.Content>
+        </Card>
+        
+        
+        main = <Table basic='very' celled collapsing>
             <Table.Header>
               <Table.Row>
                 <Table.HeaderCell>Candidatos</Table.HeaderCell>
                 <Table.HeaderCell>Votos</Table.HeaderCell>
               </Table.Row>
             </Table.Header>
-        
             <Table.Body>
-              <Table.Row>
+            {this.state.candidates.map(function(item) {
+                return <Table.Row>
                 <Table.Cell>
                   <Header as='h4' image>
-                    <Image src='https://react.semantic-ui.com/images/avatar/small/matthew.png' rounded size='mini' />
+                    <Image src={"https://react.semantic-ui.com/images/avatar/small/"+names[(Math.random() * names.length) | 0] +".png"} rounded size='mini' />
                     <Header.Content>
-                      {this.state.candidate1.name}
-                      <Header.Subheader>{this.state.candidate1.party}</Header.Subheader>
+                      {item.name}
+                      <Header.Subheader>{item.party}</Header.Subheader>
                     </Header.Content>
                   </Header>
                 </Table.Cell>
-                <Table.Cell>{this.state.candidate1.voteCount}</Table.Cell>
-              </Table.Row>
-              <Table.Row>
-                <Table.Cell>
-                  <Header as='h4' image>
-                    <Image src='https://react.semantic-ui.com/images/avatar/small/mark.png' rounded size='mini' />
-                    <Header.Content>
-                    {this.state.candidate2.name}
-                      <Header.Subheader>{this.state.candidate2.party}</Header.Subheader>
-                    </Header.Content>
-                  </Header>
-                </Table.Cell>
-                <Table.Cell>{this.state.candidate2.voteCount}</Table.Cell>
-              </Table.Row>
+                <Table.Cell>{item.voteCount}</Table.Cell>
+              </Table.Row>;
+                }) }
+        
             </Table.Body>
-          </Table>
+          </Table>;
+          } else {
+            main = <div className={styles.content}>
+                <Header as='h3'> Eleições ainda não encerradas.</Header>
+                <Header as='h3'> Por favor retorne mais tarde para obter os resultados.</Header>
+            </div>
+          }
+        return (
+            <div className={styles.content}>
+                {candidateHeader}
+                {main}
+            </div>
         )
     }
 }
--- webapp/src/components/VoteForm/VoteForm.js
+++ webapp/src/components/VoteForm/VoteForm.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react'
-import { Button, Form } from 'semantic-ui-react'
+import { Button, Form, Card, Placeholder, Image  } from 'semantic-ui-react'
 import { BrowserRouter as withRouter } from "react-router-dom";
+
+const data1 = [
+  {
+      "id": 0,
+      "name": "Joao",
+      "party": "PT",
+  },
+  {
+      "id": 1,
+      "name": "Otavio",
+      "party": "PSDB",
+  },
+  {
+      "id": 2,
+      "name": "Marcos",
+      "party": "CINSPER",
+
+  },
+]
 
 export default class VoteForm extends Component {
     constructor(props){
       super(props)
       this.state = {
+        candidates: null,
         candidateId: -1,
         voterId: -1
       };
     }
+
+    componentWillMount() {
+      // this.setState({candidates: data1}) // UNCOMMENT TO TEST WITHOUT SERVER-SIDE
+      fetch('http://localhost:8545/info')
+      .then(response => response.json())
+      .then(data => 
+          this.setState({candidates: data})
+          );
+    }
+
     vote = (e) => {
-      if(this.state.voterId == -1){
+      if(this.state.candidateId == -1){
         alert("ID do Eleitor não específicado")
       }else if(this.state.candidateId == -1 ){
         alert("Código do Candidato não específicado")
@@ -23,8 +53,8 @@ export default class VoteForm extends Component {
             'Content-Type': 'application/json',
           },
           body: JSON.stringify({
-            candidateId: this.state.candidateId,
-            voterId: this.state.voterId,
+            id: this.state.candidateId,
+            address: this.state.voterId,
           })
         }).then((response) => {
           if(response.status != 200){
@@ -38,12 +68,45 @@ export default class VoteForm extends Component {
       } 
     }
 
-    handlecandidateId (e) { this.setState({candidateId:e.target.value}); }
+    handlecandidateId (e) { console.log(e.target.value); this.setState({candidateId:e.target.value}); }
     handlevoterId (e) { this.setState({voterId:e.target.value}); }
 
     render() {
+      let candidateHeader =  <Card>
+      <Placeholder>
+        <Placeholder.Image square />
+      </Placeholder>
+      <Card.Content>
+        <Card.Header>
+        <Placeholder><Placeholder.Line /></Placeholder>
+        </Card.Header>
+        <Card.Meta>
+        <Placeholder><Placeholder.Line /></Placeholder>
+        </Card.Meta>
+      </Card.Content>
+    </Card>
+      let button = <Button disabled type='submit' onClick={this.vote}>Votar</Button>
+      if (this.state.candidates != null) {
+        const candidate = this.state.candidates.find( c => c.id === parseInt(this.state.candidateId,10) );
+        console.log(candidate)
+        if(candidate != null){
+          candidateHeader = <Card>
+          <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>{candidate.name}</Card.Header>
+            <Card.Meta>
+              <span className='date'>{candidate.party}</span>
+            </Card.Meta>
+          </Card.Content>
+        </Card>
+          if(this.state.voterId !== -1){
+            button = <Button type='submit' onClick={this.vote}>Votar</Button>
+          }
+        }
+      }
         return (
-
+          <div> 
+            {candidateHeader}
             <Form>
             <Form.Field>
               <label>Código do Candidato</label>
@@ -53,9 +116,10 @@ export default class VoteForm extends Component {
               <label>ID do Eleitor</label>
               <input onChange={this.handlevoterId.bind(this)}/>
             </Form.Field>
-            <Button type='submit' onClick={this.vote}>Votar</Button>
+            {button}
+            
           </Form>
-          // </div>
+          </div>
         )
     }
 }
